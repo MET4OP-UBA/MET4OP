814cd15d80f5f8a4ecb177aba264add67f0a97a4
Lena Sofía <lenasofia51@gmail.com>
2021-09-16 15:50:43 -0300

    Armé función buscar primos, le faltan detalles

--- examenes/trabajo_practico_programaciom_N1/prueba_diccionarios.py
+++ examenes/trabajo_practico_programaciom_N1/prueba_diccionarios.py
@@ -1,61 +1,55 @@
 # Le pedimos al usuario que ingrese un número, el cuál será el índice del primo que quiere encontrar
 
-indice_primo = input("Por favor ingrese un numero entero: ")
+indice_primo = input("Por favor ingrese un numero entero positivo para saber el numero primo que se encuentra en esa posición: ")
+
 try:
     indice_primo = int(indice_primo)
+
 except:
-    print("Usted no ingresó un número entero")
+    indice_primo = 0
 
-
-
-# Definimos un diccionario con los 6 primeros números primos, cuyas claves son los índices y sus valores son
-# los números primos correspondientes a esos índices
 
 diccionario_primos = {
-    1: 2,
-    2: 3,
-    3: 5,
-    4: 7,
-    5: 11,
-    6: 13
-}
-
-
-
-# Vamos a ver si ese número y su índice están en el diccionario de primos que tenemos
-
-if indice_primo in diccionario_primos.keys():
-    print(F'El primo número {indice_primo} es el {diccionario_primos[indice_primo]}')
-else:
-    pass #Acá vamos a poner la función y pasarle como parámetro nuestro número, pero hay que armar cosas antes
-
-
+        1: 2,
+        2: 3,
+        3: 5,
+        4: 7,
+        5: 11,
+        6: 13
+    }
 
 def encontrar_primo(numero_ingresado):
 
-    contador = len(diccionario_primos)
+    if numero_ingresado < 1:
+        print("Usted no ingresó un número entero positivo")
 
-    ultimo_primo = diccionario_primos[contador] + 1
+    elif numero_ingresado in diccionario_primos.keys():
+        print(F'El primo número {indice_primo} es el {diccionario_primos[indice_primo]}')
 
-    while contador != numero_ingresado:
+    else:
 
-        ultimo_primo += 1
+        contador = len(diccionario_primos)
 
-        primo = True
+        ultimo_primo = diccionario_primos[contador] + 1
 
-        for valor in diccionario_primos.values():
-            if ultimo_primo % valor == 0:
-                primo = False
-                break
+        while contador != numero_ingresado:
 
-        if primo == True:
-            contador += 1
-            diccionario_primos[contador] = ultimo_primo
+            ultimo_primo += 1
 
-    print(F'El primo número {numero_ingresado} es el {diccionario_primos[numero_ingresado]}')
+            primo = True
+
+            for valor in diccionario_primos.values():
+                if ultimo_primo % valor == 0:
+                    primo = False
+                    break
+
+            if primo == True:
+                contador += 1
+                diccionario_primos[contador] = ultimo_primo
+
+        print(F'El primo número {numero_ingresado} es el {diccionario_primos[numero_ingresado]}')
 
     return diccionario_primos
-
 
 primo_numero_x = encontrar_primo(indice_primo)
 print(diccionario_primos)
