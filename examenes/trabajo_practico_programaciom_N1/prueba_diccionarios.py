c1334f3148e2ea66a6107fb33ffd6e07247c68ef
Lena Sofía <lenasofia51@gmail.com>
2021-09-16 15:05:13 -0300

    En prueba diccionario armé la función para buscar números primos, me falta corregirle qué pasa cuándo el elemento ya está en el diccionario y/o es menor a 2

--- examenes/trabajo_practico_programaciom_N1/prueba_diccionarios.py
+++ examenes/trabajo_practico_programaciom_N1/prueba_diccionarios.py
@@ -1,54 +1,3 @@
-'''numero_ingresado = input("Por favor ingrese un numero entero: ")
-try:
-    numero_ingresado = int(numero_ingresado)
-except:
-    print("Usted no ingresó un número entero")
-
-primos = {
-    1: 2,
-    2: 3,
-    3: 5,
-    4: 7,
-    5: 11,
-    6: 13
-}
-
-def encontrar_numero_primo(numero_ingresado):
-
-    if numero_ingresado in primos:
-        print(F"El primo número {numero_ingresado}, es el número {primos[numero_ingresado]}")
-
-    else:'''
-
-
-
-
-
-#______________________________________________________________________________-
-
-
-'''es_primo = input("Ingrese un número natural: ")
-
-try:
-    es_primo = int(es_primo)
-except:
-    print("No ingresó un número natural")
-
-
-
-def ver_si_es_primo(numero):
-    #Verifica si el número es primo, dividiendolo entre los numeros enteros de 2 a si mismo -1
-    primo = True # Empezamos asumiendo que el número es primo
-
-    for numero in range(2, es_primo): # Dividimos el número por todos los números entre 2 y el número ingresado menos 1
-        if es_primo % numero == 0: # Si alguna de esas divisiones da como resto 0, definimos al numero como NO primo (primo = False)
-            primo = False
-            break # Salimos del ciclo
-
-    return primo'''
-
-#----------------------------------------------------------------------------------------------------
-
 # Le pedimos al usuario que ingrese un número, el cuál será el índice del primo que quiere encontrar
 
 indice_primo = input("Por favor ingrese un numero entero: ")
@@ -82,35 +31,31 @@ else:
 
 
 
-def ver_si_es_primo(numero):
-    primo = True
+def encontrar_primo(numero_ingresado):
 
-    if numero not in primos.values(): # O saco el if/else de arriba o saco este, los dos son al pedo
+    contador = len(diccionario_primos)
 
-        for clave, valor in diccionario_primos.items():
-            # Acá lo que quiero hacer es que vaya probando dividir por todos los números primos y que cuando se acaben
-            # le agregué 1 al último número primo que tenemos y siga buscando a partir de ese hasta el número que buscamos
-            # y una vez hecho eso vaya agregando los números primos que vaya encontrando en el camino, esa sería mi idea
+    ultimo_primo = diccionario_primos[contador] + 1
 
-            if numero % valor == 0:
+    while contador != numero_ingresado:
+
+        ultimo_primo += 1
+
+        primo = True
+
+        for valor in diccionario_primos.values():
+            if ultimo_primo % valor == 0:
                 primo = False
                 break
 
-    return primo
+        if primo == True:
+            contador += 1
+            diccionario_primos[contador] = ultimo_primo
+
+    print(F'El primo número {numero_ingresado} es el {diccionario_primos[numero_ingresado]}')
+
+    return diccionario_primos
 
 
-
-'''es_primo = input("Ingrese un número natural: ")
-
-try:
-    es_primo = int(es_primo)
-except:
-    print("No ingresó un número natural")
-
-
-respuesta_es_primo = ver_si_es_primo(es_primo)
-
-print(es_primo, respuesta_es_primo)'''
-
-
-#Prueba
+primo_numero_x = encontrar_primo(indice_primo)
+print(diccionario_primos)
