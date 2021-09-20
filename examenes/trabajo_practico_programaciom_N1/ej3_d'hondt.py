188ea6c6093e2bbf5204b3a2b749e95afbf92e4f
NSchteimberg <nschteimberg@gmail.com>
2021-09-20 10:49:32 -0300

    ej. 3 terminado (?) (quedan las anotaciones)

--- examenes/trabajo_practico_programaciom_N1/ej3_d'hondt.py
+++ examenes/trabajo_practico_programaciom_N1/ej3_d'hondt.py
@@ -1,43 +1,46 @@
 # %%
-import numpy as np
 import pandas as pd
 
 # Toda esta sección es para armar el DataFrame con el que se va a iterar
-lista_cocientes = []
-lista_partidos = []
-lista_votos = []
+info_correcta = False
 
-n_partidos = int(input("Ingrese el número de partidos: "))
+while info_correcta != True:    # Este while es para que si ver los datos ingresados te percatás de un error, puedas volver a ingresar datos
 
-for i in range(0,n_partidos):
-    lista_cocientes.append(1)
+    # Estas listas son las que se van a ir agregando con los inputs del usuario, y a partir de ellas se crea el DataFrame
+    lista_cocientes = []
+    lista_partidos = []
+    lista_votos = []
 
-for i in range(0,n_partidos):
-    partidos = str(input("Ingrese el nombre del partido en orden descendente por votos: "))
-    lista_partidos.append(partidos)
+    n_partidos = int(input("Ingrese el número de partidos: "))
 
-for i in range(0,n_partidos):
-    votos = int(input("Ingrese la cantidad de votos en orden descendente: "))
-    lista_votos.append(votos)
+    for i in range(0,n_partidos):
+        lista_cocientes.append(1)                                                                                   # Como para dividir la primera iteración necesito que divida Votos / 1, agrega un 1 a la lista de Cocientes por cada partido
+        partidos = str(input(f" {len(lista_cocientes)}) Ingrese el nombre del partido: "))                           
+        lista_partidos.append(partidos)
+        votos = int(input(f" {len(lista_cocientes)}) Ingrese la cantidad de votos de {lista_partidos[-1]}: "))
+        lista_votos.append(votos)
+ 
+    # Esta sección construye el DataFrame a partir de las listas creadas en el for anerior
+    votos_d ={"Partido": lista_partidos, "Votos": lista_votos, "Cociente": lista_cocientes}
+    votosDF = pd.DataFrame(votos_d)     
+    print(votosDF[["Partido","Votos"]])
+   
+    # Y aquí dependiendo del input del usuario se repite el proceso o continúa
+    x = input("Indique si la información presentada en la tabla anterior es correcta (si/no)")  
+    if x == "si":
+        info_correcta = True
+    elif x == "no":
+        info_correcta = False 
 
-votos_d ={"Partido": lista_partidos, "Votos": lista_votos, "Cociente": lista_cocientes}
-
-votosDF = pd.DataFrame(votos_d)     
-# Aquí ya está armado el DataFrame, lo que qquizás se podría mejorar es convertir todo lo anterior en una función que también imprima el Df,
-# así el usario puede ver si se equivocó cuando ingresó algun dato y repetir el proceso para corregirlo
 
 # Las bancas en juego son la cantidad de iteraciones que debe hacer
 bancas_en_juego = int(input("Ingrese la cantidad de bancas a repartir: "))
-
-# La idea del método D'Hondt es que divide todos los candidatos por un cociente inicial de 1, y el valor máximo se lleva la banca
-# En la siguiente iteración, el cociente del candidato que se llevó la última banca aumenta en 1. Por lo que pasaría a dividir por 2. Y así sucesivamente se van aumentando los cocientes cada vez que se consigue una banca
-# La cantidad de bancas es igual al cociente final -1 (porque las bancas comienzan en 0, y los cocientes comienzan en 1)
 
 # En cuanto al código, primero divide por el cociente, luego aumenta en 1 al cociente del mayor producto de la división, 
 # y luego vuelve a multiplicar por el cociente para que la siguiente iteración se haga dividiendo los votos originales, en lugar de votos/cociente.
 # En el medio también crea otro DF para usar su cociente para multiplicar por los votos, para que Votos no quede más grande que su número original
 
-for i in range(1,bancas_en_juego+1):
+for i in range(0,bancas_en_juego):
     
     votosDF["Votos"] = votosDF[["Votos"]].div(votosDF["Cociente"], axis=0)           # Divide Votos por su cociente (inicialmente 1)
     
@@ -48,8 +51,7 @@ for i in range(1,bancas_en_juego+1):
     
     votosDF["Votos"] = votosDF["Votos"] * votosDF_1["Cociente"]                      # Multiplica Votos por el cociente del nuevo DF
 
-# ! Luego convertir Cociente en Bancas y restarle 1
-
+#  Luego convertir Cociente en Bancas y restarle 1
 bancas_final = votosDF.rename(columns={"Cociente": "Bancas"})
 bancas_final["Bancas"] -= 1
 print(bancas_final)
